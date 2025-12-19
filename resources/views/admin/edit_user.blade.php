2bdd40d03fcef99b7160f6e4c773987471c193bc
JunJihad <junjihadbarroga@gmail.com>
2025-12-19 14:54:51 +0800

    done

--- resources/views/admin/edit_user.blade.php
+++ resources/views/admin/edit_user.blade.php
@@ -10,7 +10,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             Name :
-                            <input type="text" name="name" class="form-control input-sm" value="{{$userData->name}}" readonly required>
+                            <input type="text" name="name" class="form-control input-sm" value="{{$userData->name}}" required>
                         </div>
                         <div class="col-md-12">
                             Email :
